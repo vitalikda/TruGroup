e5eaa2a3a46b08fca357092dda51a03fc6f04274
Vitaly Davidenko <vitalikd13@gmail.com>
2018-10-14 23:56:11 +0300

    Edit navBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,7 +9,7 @@ const NavBar = () => {
     <nav className='navbar is-fixed-top' aria-label='main navigation'>
       <div className='navbar-brand'>
         <Link to='/' className='navbar-item'>
-                    Gatsby Starter Business
+          Rug Cleaning Toronto
         </Link>
         <button className='button navbar-burger' data-target='navMenu'>
           <span />
@@ -20,13 +20,60 @@ const NavBar = () => {
       <div className='navbar-menu' id='navMenu'>
         <div className='navbar-start'>
           <Link className='navbar-item' to='/pricing'>
-                        Pricing
+            Pricing
           </Link>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <Link className="navbar-link">
+              Services
+            </Link>
+            <div className="navbar-dropdown">
+              <Link className="navbar-item" to='/'>
+                Residential Carpet Cleaning
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Commercial Carpet Cleaning
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Upholstery and Furniture Cleaning
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Spot and Stain Removal
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Odor Removal
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Rug Cleaning
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Water Damage Removal
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Fire Damage Removal
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Mold and Mildew Removal
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Tile and Grout Cleaning
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Building and Renovation Cleaning
+              </Link>
+              <Link className="navbar-item" to='/'>
+                Mattress Cleaning
+              </Link>
+              <hr className="navbar-divider" />
+              <Link className="navbar-item" to='/'>
+                Other
+              </Link>
+            </div>
+          </div>
           <Link className='navbar-item' to='/about'>
-                        About
+            About
           </Link>
           <Link className='navbar-item' to='/blog'>
-                        Blog
+            Blog
           </Link>
         </div>
         <div className='navbar-end'>
@@ -36,7 +83,7 @@ const NavBar = () => {
                 <Link
                   className='button is-primary is-outlined'
                   to='/contact'>
-                                    Contact Us
+                  Contact Us
                 </Link>
               </p>
             </div>
