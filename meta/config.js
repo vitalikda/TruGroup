fa50068473f169b453883ab19db32e3398f53b1f
Vitaly Davidenko <vitalikd13@gmail.com>
2018-10-14 22:02:12 +0300

    Update config

--- meta/config.js
+++ meta/config.js
@@ -2,21 +2,21 @@
  * Created by vaibhav on 31/3/18
  */
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Rug Cleaning Toronto', // Site title.
+  siteTitleAlt: 'Rug Cleaning and Upholstery cleaning in Toronto, ON', // Alternative site title for SEO.
   siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://hungry-colden-879363.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Best quality and prices for toronto rug cleaning services. Call and compare! We provide rug, upholstery cleaning, furniture cleaning services in all Toronto and GTA area.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  disqusShortname: 'rug-cleaning', // Disqus shortname.
+  userName: ' Ahmet Kul',
+  userTwitter: 'felsefy',
+  userLocation: 'Toronto, ON, Canada',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
+  copyright: 'Copyright © Rug Cleaning Toronto 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#C8AE79;', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#FFFFFF', // Used for setting manifest background color.
 }
