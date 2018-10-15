5e0f97d92b059345abe04c0d119c383f37e60dad
Vitaly Davidenko <vitalikd13@gmail.com>
2018-10-15 15:02:52 +0300

    Add NetlifyCMS idetifier

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        enableIdentityWidget: true,
       },
     },
     `gatsby-plugin-offline`,
