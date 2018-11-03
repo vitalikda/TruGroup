607ee3ba7b43b5482c769c6fd151641c5d0549de
Vitaly Davidenko <vitalikd13@gmail.com>
2018-11-04 02:42:45 +0300

    Modify Booking form

--- src/components/Booking/index.js
+++ src/components/Booking/index.js
@@ -1,72 +1,81 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { Component } from 'react'
 
 import Flatpickr from 'react-flatpickr'
 import 'flatpickr/dist/flatpickr.css' // Path to Flatpickr's theme 'flatpickr/dist/themes/dark.css'
 
-const Booking = () => {
-  return (
-    <section>
-      <form name="booking" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+class Booking extends Component {
+  constructor() {
+    super();
 
-        <input type="hidden" name="bot-field" />
+    this.state = {
+      date: new Date()
+    };
+  }
 
-        <div className="field">
-          <label className="label">Name</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Your Name" />
+  render() {
+    const { date } = this.state;
+    
+    return (
+      <section>
+        <form name="booking" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <input type="hidden" name="bot-field" />
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="Your Name" />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control">
-            <input className="input" type="email" placeholder="Your Email" />
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                placeholder="Your Email" />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <label className="label">Date &amp; Time</label>
-          <div className="control">
-            <Flatpickr data-enable-time
-              options={{
-                enableTime: true,
-                time_24hr: true,
-                altInput: true,
-                altFormat: "H:i l, F j, Y",
-                dateFormat: "Y-m-d H:i",
-                defaultDate: new Date(),
-                minDate: "today",
-                maxDate: new Date().fp_incr(90),
-              }}
-              className="input"
-              type="text"
-              placeholder="Select Date..." />
+          <div className="field">
+            <label className="label">Date &amp; Time</label>
+            <div className="control">
+              <Flatpickr data-enable-time
+                value={ date }
+                onChange={date => { this.setState({date}) }}
+                options={{
+                  enableTime: true,
+                  time_24hr: true,
+                  altInput: true,
+                  altFormat: "H:i l, F j, Y",
+                  dateFormat: "Y-m-d H:i",
+                  defaultDate: new Date(),
+                  minDate: "today",
+                  maxDate: new Date().fp_incr(90),
+                }}
+                className="input"
+                type="text"
+                placeholder="Select Date..." />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <label className="label">Message</label>
-          <div className="control">
-            <textarea className="textarea" placeholder="Message"></textarea>
+          <div className="field">
+            <label className="label">Message</label>
+            <div className="control">
+              <textarea className="textarea" placeholder="Message"></textarea>
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <label className="checkbox">
-              <input type="checkbox" />
-              I agree to the <Link to="/">terms and conditions</Link>
-            </label>
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-link" type="submit">Submit</button>
+            </div>
+            <div className="control">
+              <button className="button is-text" type="reset">Cancel</button>
+            </div>
           </div>
-        </div>
-        <div className="field is-grouped">
-          <div className="control">
-            <button className="button is-link" type="submit">Submit</button>
-          </div>
-          <div className="control">
-            <button className="button is-text" type="reset">Cancel</button>
-          </div>
-        </div>
-      </form>
-    </section>
-  )
+        </form>
+      </section>
+    )
+  }
 }
 
 export default Booking
