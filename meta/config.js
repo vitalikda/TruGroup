2a980b32f4daaffeafebb78305d5b4ed8a9787c2
Vitaly Davidenko <vitalikd13@gmail.com>
2019-02-27 16:43:52 +0300

    Update GTM and Quriobot

--- meta/config.js
+++ meta/config.js
@@ -2,17 +2,17 @@ module.exports = {
   siteTitle: 'Tru Group', // Site title.
   siteTitleAlt: 'Scholarships in US universities.', // Alternative site title for SEO.
   siteLogo: '/icons/logo.svg', // Logo used for SEO and manifest.
-  siteUrl: 'https://trugroup.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'http://tru.co.education', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Scholarships in US universities.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
-  googleTagManagerID: 'GTM-KQ4X8N8', // GTM tracking ID.
-  disqusShortname: 'Tru-Group', // Disqus shortname.
+  googleTagManagerID: 'GTM-TP4TS68', // GTM tracking ID.
+  disqusShortname: 'tru-group', // Disqus shortname.
   userName: 'Tru Group',
   userTwitter: '',
   userLocation: 'USA',
-  userDescription: '',
+  userDescription: 'A collection of independently branded companies and social enterprises around the world with a unified goal to positively impact industry and society.',
   copyright: 'Copyright © Tru Group USA 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#5E81FE;', // Used for setting manifest and progress theme colors.
   backgroundColor: '#F5F8FF', // Used for setting manifest background color.
